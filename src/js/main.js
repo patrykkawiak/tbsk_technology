f262efce06e96332347a6590327fd628c6d12ec1
Mateusz Tryc <mateusz.tryc0@gmail.com>
2023-06-02 13:27:20 +0200

    made the mobile and desktop navigation pretty much functionable (some hooks are left empty at the moment, we gotta think about the structure of the links and if we want any dropdowns) and responsive. made the header always responsive to navbar height, added some events listeners etc.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,44 @@
-const mobileMenuToggler = document.querySelector('.mobile__toggler')
+const mobileMenuToggler = document.querySelector('.nav__navbar--mobile-toggler')
+const mobileNavbar = document.querySelector('.nav__navbar--mobile')
+const mobileMenu = document.querySelector('.nav-mobile__menu')
+const mobileMenuItems = document.querySelectorAll('.nav-mobile__menu-content-item')
+const mobileMenuShadow = document.querySelector('.nav-mobile__menu-shadow')
+const desktopNavbar = document.querySelector('.nav__navbar--desktop')
+const header = document.querySelector('header')
+
+const positionElementsBasedOnNavbar = () => {
+  const navbarBorder = 1
+  const navbarHeight = mobileNavbar.clientHeight + navbarBorder
+  mobileMenu.style.height = `Calc(100vh - ${navbarHeight}px)`
+  header.style.top = `${navbarHeight}px`
+  header.style.minHeight = `Calc(100vh - ${navbarHeight}px)`
+}
+
+const toggleNavbar = () => {
+  const isMobile = document.body.clientWidth < 992 ? true : false
+  if(isMobile){
+    desktopNavbar.classList.add('hidden')
+    mobileNavbar.classList.remove('hidden')
+  } else {
+    mobileNavbar.classList.add('hidden')
+    desktopNavbar.classList.remove('hidden')
+  }
+}
 
 const handleMobileToggle = () => {
-  const isActive = mobileMenuToggler.getAttribute('data-active')
-  console.log(!!isActive);
-  mobileMenuToggler.setAttribute('data-active', isActive)
+  const getActive = mobileMenuToggler.getAttribute('data-active')
+  const isActive = getActive === 'true' ? true : false
+  mobileMenuToggler.setAttribute('data-active', !isActive)
+  mobileMenu.setAttribute('data-expanded', !isActive)
 }
 
 mobileMenuToggler.addEventListener('click', handleMobileToggle)
+mobileMenuItems.forEach(item => item.addEventListener('click', handleMobileToggle))
+mobileMenuShadow.addEventListener('click', handleMobileToggle)
+
+window.addEventListener('resize', () => {
+  positionElementsBasedOnNavbar()
+  toggleNavbar()
+})
+positionElementsBasedOnNavbar()
+toggleNavbar()
