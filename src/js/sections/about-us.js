13e0facc8fb6034c7a8cb73b00eb32da6d7df37e
Mateusz Tryc <mateusz.tryc0@gmail.com>
2023-06-05 11:28:10 +0200

    added full functionality to about us section.

--- src/js/sections/about-us.js
+++ src/js/sections/about-us.js
@@ -1,3 +1,32 @@
 const rotatingBlockItems = document.querySelectorAll('.rotating-block-item')
 const blockItemIndicators = document.querySelectorAll('.rotating-block-indicators .dot')
 const fadingInfoBlocks = document.querySelectorAll('.fading-info-item')
+
+const handleRotate = (index) => {
+  rotatingBlockItems[index].classList.remove('rotateShow')
+  rotatingBlockItems[index].classList.add('rotateHide')
+  blockItemIndicators[index].classList.remove('active')
+  fadingInfoBlocks[index].classList.add('not-visible')
+  let next = index + 1
+  if(next === rotatingBlockItems.length){
+    next = 0
+  }
+  rotatingBlockItems[next].classList.remove('standby')
+  rotatingBlockItems[next].classList.add('rotateShow')
+  rotatingBlockItems[next].classList.remove('rotateHide')
+  setTimeout(() => {
+    blockItemIndicators[next].classList.add('active')
+    fadingInfoBlocks[next].classList.remove('not-visible')
+  }, 500)
+  return next
+}
+
+const handleAboutUs = () => {
+  let index = 0
+  const opacityAnimationLength = 500
+  setInterval(() => {
+    index = handleRotate(index)
+  }, 4400)
+}
+
+handleAboutUs()
