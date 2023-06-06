179f68b065a82373805e07e7b711f20ad3eca258
Mateusz Tryc <mateusz.tryc0@gmail.com>
2023-06-06 15:46:37 +0200

    made visual hierarchy stick through the whole site, made some ccode more reusable, changed some tags to more semantic ones

---
+++ src/js/optimisation.js
@@ -0,0 +1,12 @@
+
+
+const renderDesktopOrMobile = () => {
+  if (document.body.clientWidth >= 992){
+    //process desktop, nav desktop 
+  } else if (document.body.clientWidth >= 768){
+    //nav desktop, process mobile
+  } else {
+    //nav mobile, process mobile
+  }
+
+}
