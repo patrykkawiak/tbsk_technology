64e0aab97a650f8bdb8526c2b2812040e7ea7caa
Mateusz Tryc <mateusz.tryc420@gmail.com>
2023-06-10 00:11:50 +0200

    ??

--- src/js/optimization.js
+++ src/js/optimization.js
@@ -9,7 +9,7 @@ const makeCustomElement = (type, classes, text) => {
   return element
 }
 
-const renderProcessMobile = (section, info) => {
+const renderProcessMobile = (section, accordions) => {
   const isMade = document.querySelector('.processes-mobile')
   const desktopVersion = document.querySelector('.processes-desktop')
   if(desktopVersion){
@@ -17,19 +17,19 @@ const renderProcessMobile = (section, info) => {
   }
   if(!isMade){
     const processesMobile = makeCustomElement('div', ['processes-mobile'])
-    const accordions = makeCustomElement('div', ['accordions'])
-    for(let i = 0; i < headings.length; i++) {
-      const accordion = makeCustomElement('div', ['accordion'])
-        const heading = makeCustomElement('button', ['accordion-heading'])
-          const arrow = makeCustomElement('i', ['bx', 'bx-down-arrow-circle'])
-          heading.append(arrow, info.headings[i])
-        const content = makeCustomElement('div', ['accordion-content'])
-          const label = makeCustomElement('h3', ['accordion-label'], info.headings[i])
-          const text = makeCustomElement('p', ['accordion-text'], info.texts[i])
-          content.append(label, text)
-        accordion.append(heading, content)
-      accordions.append(accordion)
-    }
+    const accordionsBlock = makeCustomElement('div', ['accordions'])
+      accordions.forEach(accordion => {
+        const accordion = makeCustomElement('div', ['accordion'])
+          const heading = makeCustomElement('button', ['accordion-heading'])
+            const arrow = makeCustomElement('i', ['bx', 'bx-down-arrow-circle'])
+            heading.append(arrow, accordion.heading)
+          const content = makeCustomElement('div', ['accordion-content'])
+            const label = makeCustomElement('h3', ['accordion-label'], accordion.heading)
+            const text = makeCustomElement('p', ['accordion-text'], accordion.text)
+            content.append(label, text)
+          accordion.append(heading, content)
+        accordionsBlock.append(accordion)
+      })
     processesMobile.append(accordions)
   section.append(processesMobile)
   } 
@@ -62,40 +62,141 @@ const renderProcessDestkop = (section, info) => {
   }
 }
 
+const renderProjectsMobile = (section, info) => {
+  const isMade = document.querySelector('.processes-desktop')
+  const mobileVersion = document.querySelector('.processes-mobile')
+  if(mobileVersion){
+    mobileVersion.remove()
+  }
+  if(!isMade){
+    const projectsMobile = makeCustomElement('div', ['projects-mobile-cnt'])
+    projectsInfo.forEach(project => {
+      const projectBlock = makeCustomElement('div', ['project', project.name])
+        const projectDesc = makeCustomElement('div', ['project-desc'])
+          const descHeading = makeCustomElement('div', ['desc-heading'])
+            const headingTag = makeCustomElement('small', ['heading-tag'], project.tag)
+            const headingTitle = makeCustomElement('h3', ['heading-title'], project.title)
+            descHeading.append(headingTag, headingTitle)
+          const descContent = makeCustomElement('div', ['desc-content'])
+            const contentMain = makeCustomElement('p', ['content-main'], project.content)
+            const contentDate = makeCustomElement('small', ['content-date'], project.date)
+            descContent.append(contentMain, contentDate)
+          const descBtns = makeCustomElement('div', ['desc-btns'])
+            const visitBtn = makeCustomElement('a', ['desc-btn', 'primary'])
+              visitBtn.setAttribute('href', project.visit)
+              const visitIcon = makeCustomElement('i', ['bx', 'bx-globe'])
+              visitBtn.append('Visit', visitIcon)
+            const codeBtn = makeCustomElement('a', ['desc-btn', 'secondary'])
+              codeBtn.setAttribute('href', project.code)
+              const codeIcon = makeCustomElement('i', ['bx', 'bx-code-alt'])
+              codeBtn.append('Code', codeIcon)
+            descBtns.append(visitBtn, codeBtn)
+          projectDesc.append(descHeading, descContent, descBtns)
+        const projectShowcase = makeCustomElement('div', ['project-showcase'])
+          const showcaseImg = makeCustomElement('img', ['showcase-img'])
+            showcaseImg.setAttribute('alt', `Image of ${project.title}'s header`)
+            showcaseImg.setAttribute('src', `./dist/img/${project.name}.png`)
+          projectShowcase.append(showcaseImg)
+        project.append(projectDesc, projectShowcase)
+      projectsMobile.append(projectBlock)
+    })
+    section.append(projectsMobile)
+  }
+}
+
 const renderDesktopOrMobile = () => {
   const navbar = document.querySelector('.navbar')
   const processes = document.querySelector('.processes')
   const projects = document.querySelector('.projects')
-  const processesInfo = {
-    headings: [
-      'Jak przebiegają rozmowy',
-      'Jak przebiegają rozmowy',
-      'Jak przebiegają rozmowy',
-      'Jak przebiegają rozmowy'
-    ],
-    texts: [
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt vel eligendi consequuntur voluptas nesciunt perferendis qui id obcaecati repellendus quaerat accusantium exercitationem non, alias assumenda, sed illo totam eos dolorum.',
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt vel eligendi consequuntur voluptas nesciunt perferendis qui id obcaecati repellendus quaerat accusantium exercitationem non, alias assumenda, sed illo totam eos dolorum.',
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt vel eligendi consequuntur voluptas nesciunt perferendis qui id obcaecati repellendus quaerat accusantium exercitationem non, alias assumenda, sed illo totam eos dolorum.',
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt vel eligendi consequuntur voluptas nesciunt perferendis qui id obcaecati repellendus quaerat accusantium exercitationem non, alias assumenda, sed illo totam eos dolorum.'
-    ],
-    buttons: [
-      'Konsultacja',
-      'Prezentacja',
-      'Zatwierdzenie',
-      'Wykonanie'
-    ]
-  }
+  //TRZEBA ZMIENIC TUTJA !!!!
+  const processesInfo = [
+      {
+        heading: 'Jak przebiegają rozmowy',
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt vel eligendi consequuntur voluptas nesciunt perferendis qui id obcaecati repellendus quaerat accusantium exercitationem non, alias assumenda, sed illo totam eos dolorum.',
+        button: 'Konsultacja',
+      },
+      {
+        heading: 'Jak przebiegają rozmowy',
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt vel eligendi consequuntur voluptas nesciunt perferendis qui id obcaecati repellendus quaerat accusantium exercitationem non, alias assumenda, sed illo totam eos dolorum.',
+        button: 'Prezentacja',
+      },
+      {
+        heading: 'Jak przebiegają rozmowy',
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt vel eligendi consequuntur voluptas nesciunt perferendis qui id obcaecati repellendus quaerat accusantium exercitationem non, alias assumenda, sed illo totam eos dolorum.',
+        button: 'Zatwierdzenie',
+      },
+      {
+        heading: 'Jak przebiegają rozmowy',
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt vel eligendi consequuntur voluptas nesciunt perferendis qui id obcaecati repellendus quaerat accusantium exercitationem non, alias assumenda, sed illo totam eos dolorum.',
+        button: 'Wykonanie',
+      }
+  ]
+  const projectsInfo = [
+    {
+      name: 'power',
+      title: 'Power Kwidzyn',
+      tag: 'Built',
+      date: '20.07.2022',
+      visit: '#',
+      code: '#',
+      content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum nisi voluptatibus natus delectus numquam rem dolore beatae excepturi voluptas eveniet?'
+    },
+    {
+      name: 'rebax',
+      title: 'Rebax',
+      tag: 'Built',
+      date: '20.07.2022',
+      visit: '#',
+      code: '#',
+      content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum nisi voluptatibus natus delectus numquam rem dolore beatae excepturi voluptas eveniet?'
+    },
+    {
+      name: 'autoclinic',
+      title: 'Autoclinic',
+      tag: 'Built',
+      date: '20.07.2022',
+      visit: '#',
+      code: '#',
+      content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum nisi voluptatibus natus delectus numquam rem dolore beatae excepturi voluptas eveniet?'
+    },
+    {
+      name: 'journeyquest',
+      title: 'Journeyquest',
+      tag: 'Built',
+      date: '20.07.2022',
+      visit: '#',
+      code: '#',
+      content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum nisi voluptatibus natus delectus numquam rem dolore beatae excepturi voluptas eveniet?'
+    },
+    {
+      name: 'jumpsole',
+      title: 'JumpSole',
+      tag: 'Built',
+      date: '20.07.2022',
+      visit: '#',
+      code: '#',
+      content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum nisi voluptatibus natus delectus numquam rem dolore beatae excepturi voluptas eveniet?'
+    },
+    {
+      name: 'greenscape',
+      title: 'GreenScape',
+      tag: 'Built',
+      date: '20.07.2022',
+      visit: '#',
+      code: '#',
+      content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum nisi voluptatibus natus delectus numquam rem dolore beatae excepturi voluptas eveniet?'
+    }
+  ]
 
   if (document.body.clientWidth >= 992){
     //nav desktop, projects desktop
-    renderProcessDestkop(processes, processesHeadings, processesTexts, processButtons)
+    renderProcessDestkop(processes, processesInfo)
   } else if (document.body.clientWidth >= 768){
     //process mobile, projects mobile
-    renderProcessMobile(processes, processesHeadings, processesTexts)
+    renderProcessMobile(processes, processesInfo)
   } else {
     //process mobile, projects mobile
-     renderProcessMobile(processes, processesHeadings, processesTexts)
+     renderProcessMobile(processes, processesInfo)
   }
 }
 
