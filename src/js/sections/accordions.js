b694dcdcf277389cd27043a160163dcc08896bba
PatrykKawiak <patryk.kawiak@hotmail.com>
2023-06-12 16:48:48 +0200

    fixed accordions

--- src/js/sections/accordions.js
+++ src/js/sections/accordions.js
@@ -1,38 +1,50 @@
-let accordions = document.querySelectorAll('.accordion')
-let accordionContents = document.querySelectorAll('.accordion-content')
-console.log(accordions);
+let accordions = document.querySelectorAll('.accordion');
+let accordionContents = document.querySelectorAll('.accordion-content');
 
 const prepAccordions = () => {
-  accordionContents.forEach(content => {
-    content.style.maxHeight = '0';
-  })
-}
+	accordionContents.forEach((content) => {
+		content.style.maxHeight = '0';
+	});
+};
 
 const handleAccordionContent = (index) => {
-  const accordionContent = accordionContents[index]
+	const accordionContent = accordionContents[index];
 	if (accordionContent.style.maxHeight != '0px') {
-		accordionContent.style.maxHeight = '0px'
+		accordionContent.style.maxHeight = '0px';
 	} else {
-		closeAllAccordions()
-    const accordionHeight = accordionContents[index].scrollHeight
-		accordionContent.style.maxHeight = `${accordionHeight}px`
+		closeAllAccordions();
+		const accordionHeight = accordionContents[index].scrollHeight;
+		accordionContent.style.maxHeight = `${accordionHeight}px`;
 	}
-}
+};
+
+const outsideClick = (e) => {
+	if (
+		e.target.classList.contains('accordion-heading') ||
+		e.target.classList.contains('accordion-label') ||
+		e.target.classList.contains('accordion-text')
+	) {
+		return
+	} else {
+		closeAllAccordions();
+	}
+};
 
 const closeAllAccordions = () => {
-	accordionContents.forEach(el => {
-		el.style.maxHeight = '0'
-	})
-}
+	accordionContents.forEach((el) => {
+		el.style.maxHeight = '0';
+	});
+};
 
 accordions.forEach((accordion, index) => {
-  accordion.addEventListener('click', () => {
-    handleAccordionContent(index)
-  })
-})
+	accordion.addEventListener('click', () => {
+		handleAccordionContent(index);
+	});
+});
 
 window.addEventListener('resize', () => {
-  accordions = document.querySelectorAll('.accordion')
-  accordionContents = document.querySelectorAll('.accordion-content')
-})
-prepAccordions()
+	accordions = document.querySelectorAll('.accordion');
+	accordionContents = document.querySelectorAll('.accordion-content');
+});
+window.addEventListener('click', outsideClick);
+prepAccordions();
